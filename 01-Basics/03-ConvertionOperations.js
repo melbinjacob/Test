011877329663b6d173f21bf7b7e7c2b0096df8d0
Mel.b <68156268+melbinjacob@users.noreply.github.com>
2023-11-08 17:36:46 +0530

    Operations

--- 01-Basics/03-ConvertionOperations.js
+++ 01-Basics/03-ConvertionOperations.js
@@ -1,3 +1,5 @@
+// ***************************** DATA CONVERTION **********************************
+
 // let score = 33;
 // let score = "33";
 // let score = "33abc";
@@ -7,20 +9,23 @@
 let score = false;
 
 // console.log(typeof score);
-
 // let valueInNumber = Number(score);
 
 // console.log(typeof valueInNumber);
 // console.log(valueInNumber);
 
-// *************************NOTES*******************************
+
+
+// ************************* NOTES *******************************
 // "33" => 33
 // "33abc" => NaN
 // null => 0
 // undefined => NaN
 // true => 1
 // false => 0
-// **********************END OF NOTES***************************
+// ********************** END OF NOTES ***************************
+
+
 
 // let isLoggedIn = 1
 // let isLoggedIn = ""
@@ -30,15 +35,64 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 // console.log(typeof booleanIsLoggedIn)
 // console.log(booleanIsLoggedIn)
 
-// *************************NOTES*******************************
+
+
+// ************************* NOTES *******************************
 // 1 => true
 // 0 => false
 // "" => false
 // "Mel" => false
-// **********************END OF NOTES***************************
+// ********************** END OF NOTES ***************************
+
+
 
 let someNumber = 33;
 let stirngNumber = String(someNumber)
 
-console.log(typeof(stirngNumber));
-console.log(stirngNumber);
+// console.log(typeof(stirngNumber));
+// console.log(stirngNumber);
+
+
+
+// ***************************** OPERATIONS **********************************
+
+let value = 3;
+let negValue = -value
+
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2**2); // Two to the power of two
+// console.log(2**3); // Two to the power of three
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "Hello"
+let str2 = " Mel"
+let str3 = str1 + str2
+
+// console.log(str3);
+
+// console.log( 1 + "2");
+// console.log( "1" + 3);
+// console.log( 1 + "2" + 4);
+// console.log( 1 + 2 + "4");
+
+// console.log(+true);
+// console.log(+"");
+
+
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2;
+
+
+let gameCounter = 100;
+gameCounter++
+++gameCounter
+
+// console.log(gameCounter);
