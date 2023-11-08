78a3b216f0c3ed34a16a16102260cf08d316fe6f
Mel.b <68156268+melbinjacob@users.noreply.github.com>
2023-11-08 12:52:05 +0530

    Conversion of data types

---
+++ 01-Basics/03-ConvertionOperations.js
@@ -0,0 +1,44 @@
+// let score = 33;
+// let score = "33";
+// let score = "33abc";
+// let score = null;
+// let score = undefined;
+// let score = true;
+let score = false;
+
+// console.log(typeof score);
+
+// let valueInNumber = Number(score);
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// *************************NOTES*******************************
+// "33" => 33
+// "33abc" => NaN
+// null => 0
+// undefined => NaN
+// true => 1
+// false => 0
+// **********************END OF NOTES***************************
+
+// let isLoggedIn = 1
+// let isLoggedIn = ""
+let isLoggedIn = "Mel"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(typeof booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// *************************NOTES*******************************
+// 1 => true
+// 0 => false
+// "" => false
+// "Mel" => false
+// **********************END OF NOTES***************************
+
+let someNumber = 33;
+let stirngNumber = String(someNumber)
+
+console.log(typeof(stirngNumber));
+console.log(stirngNumber);
