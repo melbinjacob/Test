9483c8ba40a22c727d56fb503e94e2d02c71f22b
Mel.b <68156268+melbinjacob@users.noreply.github.com>
2023-11-09 10:48:28 +0530

    Equity and comparison operators

---
+++ 01-Basics/04_Comparisons.js
@@ -0,0 +1,29 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<=1);
+// console.log(2!=1);
+// console.log(2==1);
+
+// console.log("2" > 1);
+ 
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null <= 0); 
+
+
+
+// ***************************** NOTES ********************************
+
+// Unlike equity checks (==), comparison operators (<,>,<=,>=) converts null into a number "0" thats why null <= 0 and null >= 0 is shown true.
+
+// ************************** END OF NOTES ****************************
+
+
+// console.log( undefined == 0);
+// console.log( undefined <= 0);
+// console.log( undefined >= 0);
+
+// ***************************** STRICT EQUITY CHECK ********************************
+// 
+
+console.log("2" === 2);
